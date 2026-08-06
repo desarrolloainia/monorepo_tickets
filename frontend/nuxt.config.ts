f6e12f4478ff3df228bc04c6ef358e261e965655
PabSdev <desarrollo@ainia.es>
2026-08-06 08:34:15 +0200

    feat(icon): icono aplicado

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  app: {
+    head: {
+      link: [{ rel: 'icon', href: '/favicon.ico' }]
+    }
+  },
   srcDir: 'src',
   css: ['@/app/styles/main.css'],
   runtimeConfig: {
